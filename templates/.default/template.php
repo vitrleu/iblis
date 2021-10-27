d7a80c337185361d6975d82bd076ec9b970f34eb
VICTOR NAUMOV <70259531+vitrleu@users.noreply.github.com>
2021-10-27 13:45:08 +0545

    Create template.php

---
+++ templates/.default/template.php
@@ -0,0 +1,30 @@
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<ul class="nav nav-tabs">
+    <?php foreach($arResult['years'] as $year):?>
+    <li class="nav-item">
+        <a class="nav-link<?if($year == $arResult['current']):?> active" aria-current="page<?endif?>" href="?year=<?=$year?>"><?=$year?></a>
+    </li>
+  <?php endforeach?>
+</ul>
+
+<div class="tab-content m-3">
+    <?php foreach($arResult['items'] as $item):?>
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+              <img src="<?=$item['img']?>" class="img-fluid rounded-start" alt="<?=$item['name']?>">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?=$item['name']?></h5>
+                    <p class="card-text"><?=$item['text']?></p>
+                    <p class="card-text"><small class="text-muted"><?=$item['date']?></small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endforeach?>
+</div>
+
+<?=$arResult['nav']?>
