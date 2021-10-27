9f46072023726c2a9c784defc36e6bbe768ebea4
VICTOR NAUMOV <70259531+vitrleu@users.noreply.github.com>
2021-10-27 13:43:50 +0545

    Create .parameters.php

---
+++ lang/ru/.parameters.php
@@ -0,0 +1,6 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$MESS["EXAMPLE_COMPSIMPLE_PROP_SETTINGS"] = "Выбор инфоблока и настройка пейджера";
+$MESS["EXAMPLE_COMPSIMPLE_PROP_IBLOCK_TYPE"] = "Тип инфоблока";
+$MESS["EXAMPLE_COMPSIMPLE_PROP_IBLOCK_ID"] = "Инфоблок";
+$MESS["EXAMPLE_COMPSIMPLE_PROP_PAGER"] = "Число новостей на 1-ой странице";
