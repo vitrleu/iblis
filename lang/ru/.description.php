bb512dab72e006f8834e1ca6203e7772c30ef740
VICTOR NAUMOV <70259531+vitrleu@users.noreply.github.com>
2021-10-27 13:42:19 +0545

    Create .description.php

---
+++ lang/ru/.description.php
@@ -0,0 +1,8 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$MESS["EXAMPLE_COMPSIMPLE_COMPONENT_PATH_ID"] = "local";
+$MESS["EXAMPLE_COMPSIMPLE_COMPONENT_CHILD_PATH_ID"] = "iblispath";
+$MESS["EXAMPLE_COMPSIMPLE_COMPONENT_PATH_NAME"] = "Компоненты проекта";
+$MESS["EXAMPLE_COMPSIMPLE"] = "Инфоблоки";
+$MESS["EXAMPLE_COMPSIMPLE_COMPONENT"] = "Список элементов инфоблока D7";
+$MESS["EXAMPLE_COMPSIMPLE_COMPONENT_DESCRIPTION"] = "Облегченный компонет Битрикс для вывода элементов инфоблока с постраничкой и базовой фильтрацией. Класс полностью основан на ядре D7";
